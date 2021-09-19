b5203627fe2320d1f956afb057d9f99088b515e8
Sumit Puri <purisumit57@gmail.com>
2021-09-19 14:23:52 +0530

    added controller and router

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express=require('express');
 const app=express();
 const port=8000;
 
+//use express router ,this will connect this file to router
+app.use('/',require('./routes/index'));
+
 app.listen(port,function(err){
     if(err)
     {

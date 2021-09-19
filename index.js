9cb85592a2c68ce85b17022167927f17d9c60342
Sumit Puri <purisumit57@gmail.com>
2021-09-20 00:29:59 +0530

    added styling to layouts

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express=require('express');
 const app=express();
 const port=8000;
+const expressLayouts=require('express-ejs-layouts');
 const db=require('./config/mongoose');
+
+
+app.use(express.static('./assets'));
+app.use(expressLayouts);
+
 //use express router ,this will connect this file to router
 app.use('/',require('./routes/index'));
+
 
 //setting up view engine and providing view file
 app.set('view engine','ejs');
 app.set('views','./views');
+
 
 app.listen(port,function(err){
     if(err)

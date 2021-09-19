1316706d622ee58c79ca4d62e43462677a75b0e9
Sumit Puri <purisumit57@gmail.com>
2021-09-19 15:13:11 +0530

    added view engine

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port=8000;
 //use express router ,this will connect this file to router
 app.use('/',require('./routes/index'));
 
+//setting up view engine and providing view file
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err)
     {

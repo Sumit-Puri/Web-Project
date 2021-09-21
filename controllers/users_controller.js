760b875a9efba069099b2a17e43c289577c81474
Sumit Puri <purisumit57@gmail.com>
2021-09-21 22:57:18 +0530

    signing up users

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,4 @@
+const User=require('../models/user');
 module.exports.profile = function(req,res){
     return res.render('user_profile',{
         title: "user-profile"
@@ -16,7 +17,32 @@ module.exports.signIn=function(req,res){
 }
 //getting signup data 
 module.exports.create = function(req,res){
-    //getting form data
+    if(req.body.password!=req.body.confirm_password)
+    {
+        return res.redirect('back');
+    }
+    User.findOne({email: req.body.email},function(err,user){
+        if(err)
+        {
+            console.log('error in fining user'); 
+            return;
+        }
+        if(!user)
+        {
+            User.create(req.body,function(err,user){
+                if(err)
+                {
+                    console.log('error in finding user');
+                    return;
+                }
+                return res.redirect('/users/signin');
+            })
+        }
+        else
+        {
+            return res.redirect('back');
+        }
+    })
 }
 //getting signin form data
 module.exports.createSession = function(req,res){
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,5 @@ const user_Controller = require('../controllers/users_controller');
 router.get('/profile',user_Controller.profile);
 router.get('/signup',user_Controller.signUp);
 router.get('/signin',user_Controller.signIn);
+router.post('/create',user_Controller.create);
 module.exports= router;

a455715f9c620a9f82fec6dcdac74a470779fea0
Sumit Puri <purisumit57@gmail.com>
2021-09-30 00:33:35 +0530

    async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,7 @@
 const { populate } = require('../models/post');
 const Post=require('../models/post');
 const User = require('../models/user');
-module.exports.home= function(req,res){
+module.exports.home= async function(req,res){
     // Post.find({},function(err,posts){
     //     return res.render('home',{
     //         title: "MY HOME",
@@ -9,21 +9,37 @@ module.exports.home= function(req,res){
     //     });
     // });
     //populate user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err,posts){
-        User.find({},function(err,users){
-            return res.render('home',{
-                title: "MY HOME",
-                posts: posts,
-                all_users:users
-            });
+
+    try{
+            let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
         });
-    });
+
+
+        let users=await User.find({});
+        
+        return res.render('home',{
+            title: "MY HOME",
+            posts: posts,
+            all_users:users
+        });
+    }catch(err){
+        console.log('Error',err);
+        return ;
+    }
+    
+    // .exec(function(err,posts){
+    //     User.find({},function(err,users){
+    //         return res.render('home',{
+    //             title: "MY HOME",
+    //             posts: posts,
+    //             all_users:users
+    //         });
+    //     });
+    // });
 }
